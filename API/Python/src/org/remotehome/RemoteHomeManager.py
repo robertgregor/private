a84dfdd19b4e8f8465b3d6f9645e2bb62a546762
David Rehak <david.rehak@remote-home.org>
2012-12-14 17:11:30 +0100

    Python API

---
+++ API/Python/src/org/remotehome/RemoteHomeManager.py
@@ -0,0 +1,12 @@
+'''
+Created on 14.12.2012
+
+@author: wg372
+'''
+
+class RemoteHomeManager(object):
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
---
+++ API/Python/src/org/remotehome/__init__.py
@@ -0,0 +1,5 @@
+'''
+Created on 14.12.2012
+
+@author: wg372
+'''
