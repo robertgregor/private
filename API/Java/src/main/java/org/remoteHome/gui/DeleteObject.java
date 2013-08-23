4c8d409c87cc7a86ad0dfae4507bd9cd26d5ffbb
David Rehak <david.rehak@remote-home.org>
2013-08-23 13:56:40 +0200

    DeleteObject

--- API/Java/src/main/java/org/remoteHome/gui/DeleteObject.java
+++ API/Java/src/main/java/org/remoteHome/gui/DeleteObject.java
@@ -27,8 +27,8 @@ public class DeleteObject extends AbstractWebService {
         String loadData = requestParameters.get("loadData");
         String loadDevices = requestParameters.get("loadDevices");
         String loadRooms = requestParameters.get("loadRooms");
-        String deleteDevice = requestParameters.get("deleteDeviceId");
-        String deleteRoom = requestParameters.get("deleteRoomId");
+        String deleteDevice = requestParameters.get("deleteDeviceName");
+        String deleteRoom = requestParameters.get("deleteRoomName");
 
         try {
             if (loadData != null && loadData.equals("true")) {
@@ -40,7 +40,7 @@ public class DeleteObject extends AbstractWebService {
                         Room r = new Room(roomName);
                         rooms.add(r);
                     }
-                    System.out.println(AbstractDevice.generateJsonData(rooms));
+                    //System.out.println(AbstractDevice.generateJsonData(rooms));
                     sendAjaxAnswer(AbstractDevice.generateJsonData(rooms));
                 } else if(loadDevices != null && loadDevices.equals("true")
                             && loadRooms != null && loadRooms.equals("false")) {
@@ -50,30 +50,30 @@ public class DeleteObject extends AbstractWebService {
                         Device device = new Device(dev.getDeviceName(), dev.getDeviceId());
                         devices.add(device);
                     }
-                    System.out.println(AbstractDevice.generateJsonData(devices));
+                    //System.out.println(AbstractDevice.generateJsonData(devices));
                     sendAjaxAnswer(AbstractDevice.generateJsonData(devices));
                 } else {
                     sendAjaxAnswer("Something went wrong.");
                 }
             } else {
-                if(deleteDevice != null && !deleteDevice.equals("")
-                    && deleteRoom != null && deleteRoom.equals("")) {
+                if(deleteDevice != null && !deleteDevice.equals("0")
+                    && deleteRoom != null && deleteRoom.equals("0")) {
                     try {
                         r.removeDevice(Integer.valueOf(deleteDevice));
                     } catch (NumberFormatException e) {
                         sendAjaxAnswer("Not a valid DeviceId");
                     }
                     sendAjaxAnswer("Deleted device: " + deleteDevice);
-                } else if(deleteDevice != null && deleteDevice.equals("")
-                            && deleteRoom != null && !deleteRoom.equals("")) {
+                } else if(deleteDevice != null && deleteDevice.equals("0")
+                            && deleteRoom != null && !deleteRoom.equals("0")) {
                     for (AbstractDevice dev : r.getDevices()) {
                         if(dev.getRoomName().equals(deleteRoom)) {
                             r.removeDevice(dev.getDeviceId());
                         }
                     }
                     sendAjaxAnswer("Deleted room: " + deleteRoom);
-                } else if(deleteDevice != null && !deleteDevice.equals("")
-                        && deleteRoom != null && !deleteRoom.equals("")) {
+                } else if(deleteDevice != null && !deleteDevice.equals("0")
+                        && deleteRoom != null && !deleteRoom.equals("0")) {
                     try {
                         r.removeDevice(Integer.valueOf(deleteDevice));
                         for (AbstractDevice dev : r.getDevices()) {
