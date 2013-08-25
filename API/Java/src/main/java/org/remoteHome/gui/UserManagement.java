1540f8063c8bc07d010715bb5d29d5c1a6739305
David Rehak <david.rehak@remote-home.org>
2013-08-25 18:40:19 +0200

    Updates.

---
+++ API/Java/src/main/java/org/remoteHome/gui/UserManagement.java
@@ -0,0 +1,26 @@
+package org.remoteHome.gui;
+
+import com.sun.net.httpserver.HttpExchange;
+import org.remoteHome.RemoteHomeManagerException;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dr
+ * Date: 8/25/13
+ * Time: 6:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserManagement extends AbstractWebService {
+
+    public void init() {
+
+    }
+
+    public void processRequest(OutputStream o, HttpExchange t) throws IOException, RemoteHomeManagerException {
+
+    }
+
+}
