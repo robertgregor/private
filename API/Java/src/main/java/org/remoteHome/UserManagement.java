e187c9ee037c4d3d90e44c068984016e302d9537
David Rehak <david.rehak@remote-home.org>
2013-08-28 20:10:19 +0200

    Logon

--- API/Java/src/main/java/org/remoteHome/UserManagement.java
+++ API/Java/src/main/java/org/remoteHome/UserManagement.java
@@ -1,5 +1,6 @@
 package org.remoteHome;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
@@ -15,11 +16,12 @@ import java.util.List;
 public class UserManagement {
 
     private List<User> users = new ArrayList<User>();
-    public static User ADMIN = new User(0, "admin",  "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", Group.ADMIN_GROUP, false, null);
+    public static User ADMIN = new User(0, "admin",  "admin", Group.ADMIN_GROUP, false, null);
     private boolean defaultAdminPassword;
 
     public void init() {
         setDefaultAdminPassword(true);
+        ADMIN.setPassword(computeSha1OfString(ADMIN.getPassword())); // 8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918
         users.add(ADMIN);
     }
 
@@ -39,9 +41,55 @@ public class UserManagement {
         this.defaultAdminPassword = defaultAdminPassword;
     }
 
-    public boolean passwordsAreEqual(String password1, String password2) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        return md.digest(password1.getBytes()).equals(md.digest(password2.getBytes()));
+    /**
+     * Lower case Hex Digits.
+     */
+    private static final String HEX_DIGITS = "0123456789abcdef";
+
+    /**
+     * Byte mask.
+     */
+    private static final int BYTE_MSK = 0xFF;
+
+    /**
+     * Hex digit mask.
+     */
+    private static final int HEX_DIGIT_MASK = 0xF;
+
+    /**
+     * Number of bits per Hex digit (4).
+     */
+    private static final int HEX_DIGIT_BITS = 4;
+
+    public static String computeSha1OfString(final String message)
+            throws UnsupportedOperationException, NullPointerException {
+        try {
+            return computeSha1OfByteArray(message.getBytes(("UTF-8")));
+        } catch (UnsupportedEncodingException ex) {
+            throw new UnsupportedOperationException(ex);
+        }
+    }
+
+    private static String computeSha1OfByteArray(final byte[] message)
+            throws UnsupportedOperationException {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            md.update(message);
+            byte[] res = md.digest();
+            return toHexString(res);
+        } catch (NoSuchAlgorithmException ex) {
+            throw new UnsupportedOperationException(ex);
+        }
+    }
+
+    private static String toHexString(final byte[] byteArray) {
+        StringBuilder sb = new StringBuilder(byteArray.length * 2);
+        for (int i = 0; i < byteArray.length; i++) {
+            int b = byteArray[i] & BYTE_MSK;
+            sb.append(HEX_DIGITS.charAt(b >>> HEX_DIGIT_BITS)).append(
+                    HEX_DIGITS.charAt(b & HEX_DIGIT_MASK));
+        }
+        return sb.toString();
     }
 
 }
